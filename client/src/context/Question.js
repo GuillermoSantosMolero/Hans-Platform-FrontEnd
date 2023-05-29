f5b4108a70749fc2e3f9544dbf808083447544d6
GuillermoSantosMolero <115094288+GuillermoSantosMolero@users.noreply.github.com>
2023-05-29 18:26:58 +0200

    Hans 0.2
    Sólo la build

--- client/src/context/Question.js
+++
@@ -1,8 +0,0 @@
-
-const QuestionStatus = Object.freeze({
-    Undefined: Symbol("undefined"), // No question defined
-    Loading: Symbol("loading"),     // Question ID defined, but details were not retrieved yet
-    Loaded: Symbol("loaded"),       // Question fully loaded, all details are available
-});
-
-export { QuestionStatus };
